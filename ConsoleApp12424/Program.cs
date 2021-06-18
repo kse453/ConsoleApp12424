2992b3af7bfb99d3f79019768ab7f58c8a4c2ea9
Cab38-02 <Cab38-02@03-C38-02>
2021-06-18 12:58:32 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp12424/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp12424
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = 10;
+            Random r = new Random();
+            int[] array = new int[20];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = r.Next(100);
+                Console.Write(array[i] + " ");
+            }
+            int max = array[0], index = -1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (max < array[i])
+                {
+                    max = array[i];
+                    index = i;
+                }
+            }
+            array[index] = n;
+            Console.WriteLine();
+            for (int i = 0; i < array.Length; i++) Console.Write(array[i] + " ");
+        }
+    }
+}
